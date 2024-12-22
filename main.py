87126b6f35f609a93bd5da10ffadf1395a0effc8
Dragos Rudencu <dragosrudencu24@gmail.com>
2024-12-22 18:44:08 +0200

    started training

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ matches.head()
 matches["team"].value_counts()
 # print(matches[matches["team"] == "Liverpool"])
 # print(matches["round"].value_counts())
+
+# matches.dtypes
+
+matches["date"] = pd.to_datetime(matches["date"])
+
+# ML part
+matches["venue_code"] = matches["venue"].astype("category").cat.codes
+matches["opp_code"] = matches["opponent"].astype("category").cat.codes
+matches["hour"] = matches["time"].str.replace(":.+", "", regex=True).astype("int")
+matches["day_code"] = matches["date"].dt.dayofweek
+matches["target"] = (matches["result"] == "W").astype("int")
+print(matches)
