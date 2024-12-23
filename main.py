3090e0f9c04c710d73bc3e9ae1861cf87aa43a0e
Dragos Rudencu <dragosrudencu24@gmail.com>
2024-12-24 00:03:56 +0200

    improved precision with rolling averages

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ matches["target"] = (matches["result"] == "W").astype("int")
 # print(matches)
 
 from sklearn.ensemble import RandomForestClassifier
+
 rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
 train = matches[matches["date"] < '2022-01-01']
 test = matches[matches["date"] > '2022-01-01']
@@ -30,9 +31,33 @@ print(rf.fit(train[predictors], train["target"]))
 
 preds = rf.predict(test[predictors])
 from sklearn.metrics import accuracy_score
+
 acc = accuracy_score(test["target"], preds)
 print(acc)
 
 combined = pd.DataFrame(dict(actual=test["target"], prediction=preds))
 print(pd.crosstab(index=combined["actual"], columns=combined["prediction"]))
 
+# Revising the predicting method
+from sklearn.metrics import precision_score
+
+precision_score(test["target"], preds)
+grouped_matches = matches.groupby("team")
+group = grouped_matches.get_group("Manchester City")
+
+
+def rolling_averages(group, cols, new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(3, closed='left').mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+
+
+cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+new_cols = [f"{c}_rolling" for c in cols]
+rolling_averages(group, cols, new_cols)
+matches_rolling = matches.groupby("team").apply(lambda x: rolling_averages(x, cols, new_cols))
+matches_rolling = matches_rolling.droplevel('team')
+matches_rolling.index = range(matches_rolling.shape[0])
+print(matches_rolling)
